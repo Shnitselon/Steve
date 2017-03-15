c8a0c1b88aa1283e6d87347092e161c8bc91ec2e
Shnitselon <steven.g@quali.com>
2017-03-15 22:11:02 +0200

    Update install_program.ps1

--- install_program.ps1
+++ install_program.ps1
@@ -1,4 +1,4 @@
 $url = "https://github.com/Shnitselon/Steve/blob/master/putty-0.67-installer.msi"
 $output = "c:\putty-0.67-installer.msi"
 Invoke-WebRequest -Uri $url -OutFile $output
-msiexec /i "C:\Users\admin\Downloads\putty-0.67-installer.msi" /s
+msiexec /i "c:\putty-0.67-installer.msi" /s
