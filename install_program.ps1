459dcfb0684531f2f7d08ad06ee0502ff6285a2c
Shnitselon <steven.g@quali.com>
2017-03-15 17:24:56 +0200

    Update install_program.ps1

--- install_program.ps1
+++ install_program.ps1
@@ -1,1 +1,4 @@
+$url = "http://mirror.internode.on.net/pub/test/10meg.test"
+$output = "c:\putty-0.67-installer.msi"
+Invoke-WebRequest -Uri $url -OutFile $output
 msiexec /i "C:\Users\admin\Downloads\putty-0.67-installer.msi" /s
