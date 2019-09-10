9942ccbf8d12528d4d6290f33e46b620b3a43f1c
Steve <steven.g@quali.com>
2019-09-10 16:28:23 +0300

    Delete NewUserViaPowershell..ps1

--- NewUserViaPowershell..ps1
+++
@@ -1,3 +0,0 @@
-$Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
-New-LocalUser "Demo" -Password $Pwd -FullName "Demo" -Description "Demo user account." -PasswordNeverExpires 
-Add-LocalGroupMember -Group "Administrators" -Member "Demo"
