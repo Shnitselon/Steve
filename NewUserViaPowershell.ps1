15b312575a9c2e18f821f63453aed0abef4063f3
Steve <steven.g@quali.com>
2019-09-16 21:07:32 +0300

    Update NewUserViaPowershell.ps1

--- NewUserViaPowershell.ps1
+++ NewUserViaPowershell.ps1
@@ -1,7 +1,7 @@
 Write-Output "started powershell script"
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
 Write-Output "created secure string"
-New-LocalUser "Demo" -Password $Pwd -FullName "Demo user" -Description "Demo user account." -PasswordNeverExpires
+New-LocalUser "Demo" -Password $Pwd -PasswordNeverExpires
 Write-Output "created Demo user"
 Add-LocalGroupMember -Group "Administrators" -Member "Demo"
 Write-Output "Add user to Administrators group"
