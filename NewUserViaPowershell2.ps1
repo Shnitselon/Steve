31052b1b48f8d43712fecfc4c93d81d19e99a56c
Steve <steven.g@quali.com>
2019-09-18 11:48:08 +0300

    Update NewUserViaPowershell2.ps1

--- NewUserViaPowershell2.ps1
+++ NewUserViaPowershell2.ps1
@@ -1,3 +1,15 @@
+$dir_name = "temp-logs"
+$dir_path = "C:\$dir_name\azure-config-log.txt"
+New-Item -ItemType Directory -Force -Path "C:\$dir_name"
+
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
-New-LocalUser "Demo" -Password $Pwd -FullName "Demo" -Description "Demo user account." -PasswordNeverExpires 
-Add-LocalGroupMember -Group "Administrators" -Member "Demo"
+Add-Content -Path $dir_path -Value "create password" -Force
+
+New-LocalUser "Demo11" -Password $Pwd -FullName "Demo11" -Description "Demo user account." -PasswordNeverExpires
+Add-Content -Path $dir_path -Value "create 'Demo' user" -Force
+
+Add-LocalGroupMember -Group "Administrators" -Member "Demo11"
+Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
+
+Copy-Item -Path "C:\AFIFARM\DATA\*" -Destination "C:\test" -Recurse
+Add-Content -Path $dir_path -Value "copy contents to 'C:\test'" -Force
