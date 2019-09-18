89d90588e8ba54a928f4e6583048c861ee640ff6
Steve <steven.g@quali.com>
2019-09-18 11:49:04 +0300

    Update NewUserViaPowershell2.ps1

--- NewUserViaPowershell2.ps1
+++ NewUserViaPowershell2.ps1
@@ -5,11 +5,12 @@ New-Item -ItemType Directory -Force -Path "C:\$dir_name"
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
 Add-Content -Path $dir_path -Value "create password" -Force
 
-New-LocalUser "Demo11" -Password $Pwd -FullName "Demo11" -Description "Demo user account." -PasswordNeverExpires
+New-LocalUser "Demo" -Password $Pwd -FullName "Demo" -Description "Demo user account." -PasswordNeverExpires
 Add-Content -Path $dir_path -Value "create 'Demo' user" -Force
 
-Add-LocalGroupMember -Group "Administrators" -Member "Demo11"
+Add-LocalGroupMember -Group "Administrators" -Member "Demo"
 Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
 
 Copy-Item -Path "C:\AFIFARM\DATA\*" -Destination "C:\test" -Recurse
 Add-Content -Path $dir_path -Value "copy contents to 'C:\test'" -Force
+Add-Content -Path $dir_path -Value "success!" -Force
