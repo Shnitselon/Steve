49b1cea4c85795df4847c2ba0f029b5439271c15
Steve <steven.g@quali.com>
2019-09-18 14:29:41 +0300

    Add files via upload

---
+++ NewUserViaPowershell2NoRestart.ps1
@@ -0,0 +1,20 @@
+$dir_name = "temp-logs"
+$dir_path = "C:\$dir_name\azure-config-log.txt"
+New-Item -ItemType Directory -Force -Path "C:\$dir_name"
+
+Rename-Computer -ComputerName "localhost" -NewName "Afifarm53new" -Force
+Add-Content -Path $dir_path -Value "rename machine to Afifarm53new!" -Force
+
+$Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
+Add-Content -Path $dir_path -Value "create password" -Force
+
+New-LocalUser "Demo" -Password $Pwd -FullName "Demo" -Description "Demo user account." -PasswordNeverExpires
+Add-Content -Path $dir_path -Value "create 'Demo' user" -Force
+
+Add-LocalGroupMember -Group "Administrators" -Member "Demo"
+Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
+
+Copy-Item -Path "C:\AFIFARM\DATA\*" -Destination "C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
+Add-Content -Path $dir_path -Value "copy contents to  Demo's Startup folder" -Force
+
+Add-Content -Path $dir_path -Value "success!" -Force
