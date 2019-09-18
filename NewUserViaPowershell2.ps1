08013b92e5eb616ec34b1daa0006c096bcd67a22
Steve <steven.g@quali.com>
2019-09-18 13:37:05 +0300

    Update NewUserViaPowershell2.ps1

--- NewUserViaPowershell2.ps1
+++ NewUserViaPowershell2.ps1
@@ -2,6 +2,9 @@ $dir_name = "temp-logs"
 $dir_path = "C:\$dir_name\azure-config-log.txt"
 New-Item -ItemType Directory -Force -Path "C:\$dir_name"
 
+Rename-Computer -ComputerName "localhost" -NewName "Afifarm53new" -Force -Restart
+Add-Content -Path $dir_path -Value "rename machine to Afifarm53new!" -Force
+
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
 Add-Content -Path $dir_path -Value "create password" -Force
 
@@ -12,8 +15,6 @@ Add-LocalGroupMember -Group "Administrators" -Member "Demo"
 Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
 
 Copy-Item -Path "C:\AFIFARM\DATA\*" -Destination "C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
-Add-Content -Path $dir_path -Value "copy contents to 'C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'" -Force
+Add-Content -Path $dir_path -Value "copy contents to  Demo's Startup folder" -Force
 
-Rename-Computer -ComputerName "localhost" -NewName "Afifarm53new" -Force -Restart
-Add-Content -Path $dir_path -Value "rename machine to Afifarm53new!" -Force
 Add-Content -Path $dir_path -Value "success!" -Force
