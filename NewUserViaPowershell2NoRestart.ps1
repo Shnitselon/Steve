a3b00335d40beffbe434021fc988062d57b270f2
Steve <steven.g@quali.com>
2019-09-19 10:28:37 +0300

    Update NewUserViaPowershell2NoRestart.ps1

--- NewUserViaPowershell2NoRestart.ps1
+++ NewUserViaPowershell2NoRestart.ps1
@@ -2,8 +2,6 @@ $dir_name = "temp-logs"
 $dir_path = "C:\$dir_name\azure-config-log.txt"
 New-Item -ItemType Directory -Force -Path "C:\$dir_name"
 
-Rename-Computer -ComputerName "localhost" -NewName "Afifarm53new" -Force
-Add-Content -Path $dir_path -Value "rename machine to Afifarm53new!" -Force
 
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
 Add-Content -Path $dir_path -Value "create password" -Force
@@ -16,5 +14,8 @@ Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
 
 Copy-Item -Path "C:\Users\admin\Documents\SQL Server Management Studio\*" -Destination "C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
 Add-Content -Path $dir_path -Value "copy contents to  Demo's Startup folder" -Force
+
+Add-Content -Path $dir_path -Value "restarting machine..." -Force
+Rename-Computer -ComputerName "localhost" -NewName "Afifarm53new" -Force -Restart
 
 Add-Content -Path $dir_path -Value "success!" -Force
