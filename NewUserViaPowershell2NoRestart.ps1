1dadb0e3041a1bae347c9762aa0064e547fbc854
Steve <steven.g@quali.com>
2019-09-19 10:26:19 +0300

    Update NewUserViaPowershell2NoRestart.ps1

--- NewUserViaPowershell2NoRestart.ps1
+++ NewUserViaPowershell2NoRestart.ps1
@@ -8,13 +8,13 @@ Add-Content -Path $dir_path -Value "rename machine to Afifarm53new!" -Force
 $Pwd = ConvertTo-SecureString "Afifarm15148" -AsPlainText -Force
 Add-Content -Path $dir_path -Value "create password" -Force
 
-New-LocalUser "Demo" -Password $Pwd -FullName "Demo" -Description "Demo user account." -PasswordNeverExpires
+New-LocalUser "Demo2" -Password $Pwd -FullName "Demo3" -Description "Demo user account." -PasswordNeverExpires
 Add-Content -Path $dir_path -Value "create 'Demo' user" -Force
 
-Add-LocalGroupMember -Group "Administrators" -Member "Demo"
+Add-LocalGroupMember -Group "Administrators" -Member "Demo2"
 Add-Content -Path $dir_path -Value "Assign to 'Administrators' group" -Force
 
-Copy-Item -Path "C:\AFIFARM\DATA\*" -Destination "C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
+Copy-Item -Path "C:\Users\admin\Documents\SQL Server Management Studio\*" -Destination "C:\Users\Demo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
 Add-Content -Path $dir_path -Value "copy contents to  Demo's Startup folder" -Force
 
 Add-Content -Path $dir_path -Value "success!" -Force
